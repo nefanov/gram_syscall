225abe638883bc860e2d82997d6edfe10662a18f
636task <efanov.nn@mipt.ru>
2017-01-16 17:28:39 +0300

    clean perm.

--- bruteforces.py
+++ bruteforces.py
@@ -30,37 +30,23 @@ def fork_lst_transform(argv):
 
 depth = 0
 
+
 def perm(r, rls, argv, is_log=0, log_tree=None, max_proc_num=rules.PROC_LIMIT):
-    #  ! lst is crafted pointer list
-    global depth
-    depth += 1
-    print "Recurion step: depth " + str(depth)
     lst = [node for node in routines.PreOrderIter(r)]
-    routines.log_output(r, "")
-#    print str(len(lst))
-#    print lst
     if len(lst) >= max_proc_num:
-        depth -= 1
         return
-    cnt = 0
+
     for num, node in enumerate(lst): # cycle iterates on changable list but r_branch is saved copy of this instance
-#        routines.log_output(r, "")
-        print "             num " + str(num)
-#        print num
         for rn, rule in enumerate(rls):
             tmp_r = copy.deepcopy(r)
             argv_ret = getattr(rules, rule)(node, *argv[rn])
 
             if is_log:
                 log_tree = routines.Node([rule, '('+','.join([str(x) for x in argv[rn]])+')', routines.construct(r)], parent=log_tree)
-                #print "node #" + str(num+1) + " of " + str(len(lst)) + ":" + routines.construct(r)[0] + " ; "
-#                print argv_ret
-#                routines.log_output(r, "")
 
             perm(r, rls, [[argv_ret]], is_log, log_tree, max_proc_num)
             r = tmp_r
-#            print "    leave"
-            depth -= 1
+
     return
 
 
